f051eed80eb10138a24a6015a9f602b46aed58dd
Arturo Varón <arturo.varon@gmail.com>
2022-05-30 18:00:03 +0200

    udpate

--- app/services/in-app-review.ts
+++ app/services/in-app-review.ts
@@ -7,8 +7,8 @@ export const addPositiveAction = () => {
   if(InAppReview.isAvailable()) {
     const positiveActions = (storage.getNumber(POSITIVE_ACTIONS_KEY) || 0) + 1
     storage.set(POSITIVE_ACTIONS_KEY, positiveActions)
-    // la primera, a las 35 acciones positivas, a patir de ahí cada 50 acciones positivas
-    if((positiveActions > 35 ? positiveActions % 50 : positiveActions % 35) === 0) {
+    // cada 35 acciones positivas
+    if(positiveActions % 35 === 0) {
       InAppReview.RequestInAppReview().catch(() => {});
     }
   }
